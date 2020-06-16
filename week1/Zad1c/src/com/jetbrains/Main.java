d439929a2f684a8d4bd2880fabbb2548cd92f99e
Nikol Angleova <49544444+nicole-an9elova@users.noreply.github.com>
2020-06-16 05:46:33 +0300

    Add files via upload

---
+++ week1/Zad1c/src/com/jetbrains/Main.java
@@ -0,0 +1,27 @@
+package com.jetbrains;
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new  Scanner(System.in);
+        System.out.println("What temperature do you want to convert?(c or f):");
+        char choiceOfTemp = input.next().charAt(0);
+        double fahrenheitTemp;
+        double celsiusTemp;
+        if(choiceOfTemp == 'f') {
+            System.out.print("Enter Fahrenheit temperature:");
+            fahrenheitTemp = input.nextDouble();
+            celsiusTemp = (5.0 / 9.0 * (fahrenheitTemp - 32));
+            System.out.print("This is the Celsius temperature:" + celsiusTemp);
+        }
+        else if(choiceOfTemp == 'c') {
+            System.out.print("Enter Celsius temperature:");
+            celsiusTemp = input.nextDouble();
+            fahrenheitTemp = (9.0 / 5.0 * (celsiusTemp + 32));
+            System.out.print("This is the Fahrenheit temperature:" + fahrenheitTemp);
+        }
+        else{
+            System.out.print("No such temperature! :)");
+        }
+    }
+}
